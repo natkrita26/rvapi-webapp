c53cc29bfbd2137a264697208ac62c466d58e57d
Natkrita26 <natkrita.son@dome.tu.ac.th~>
2025-03-04 12:05:08 +0700

    Add ProductList component with API connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>ร้านค้าสินค้าออนไลน์</h1>
-      <ProductList apiUrl="http://localhost:5000" />
+      <ProductList apiUrl="http://localhost:5127" />
     </div>
   );
 }
